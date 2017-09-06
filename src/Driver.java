6dfdf81c1ecd6fb3b62f663964e384780a051999
Gibbedboy <31701378+Gibbedboy@users.noreply.github.com>
2017-09-06 10:27:20 -0600

    Initial .java project setup
    Setting up the bones and base of the HW project so more can be added on later.

---
+++ src/Driver.java
@@ -0,0 +1,18 @@
+/*	
+ * 	CSCI-451 HW-01 Z-Algorithm
+ * 	Norman Tang & Chen Wei Cao
+ * 
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Driver {
+	
+	public static void main(String[] args) throws IOException
+	{
+		// Stuff will go into here.
+	}
+	
+}
